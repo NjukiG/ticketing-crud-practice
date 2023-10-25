41c9ae506c5699281f60d657ffc412cac9bed2ca
George <gnjuki19@gmail.com>
2023-10-25 13:36:59 +0300

    Still working on styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import TicketCard from './(components)/TicketCard'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <TicketCard />
+      <TicketCard />
+      <TicketCard />
+
+    </div>
   )
 }
 
