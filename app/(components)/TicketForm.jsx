5758e7a94f7e6b370cd3224e8472399a810cffb8
George <gnjuki19@gmail.com>
2023-10-26 12:26:33 +0300

    Working on the form styling

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 const TicketForm = () => {
-  return (
-    <div>TicketForm</div>
-  )
-}
+  const handleChange = (e) => {
+    // e.preventDefault();
+    const value = e.target.value;
+    const name = e.target.name;
 
-export default TicketForm
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    category: "Software Problem",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  return (
+    <div className="flex justify-center">
+      <form>
+        <h3>Create your ticket!</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
