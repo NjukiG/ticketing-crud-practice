a577057664a036f37b3a8346f52e171d8c105df8
George <gnjuki19@gmail.com>
2023-10-26 12:44:35 +0300

    Wokring on the form

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/router";
 
 const TicketForm = () => {
   const handleChange = (e) => {
-    // e.preventDefault();
     const value = e.target.value;
     const name = e.target.name;
 
@@ -13,6 +12,12 @@ const TicketForm = () => {
       ...prevState,
       [name]: value,
     }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Submitted!");
   };
 
   const startingTicketData = {
@@ -28,7 +33,11 @@ const TicketForm = () => {
 
   return (
     <div className="flex justify-center">
-      <form>
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <h3>Create your ticket!</h3>
         <label>Title</label>
         <input
@@ -39,6 +48,80 @@ const TicketForm = () => {
           required={true}
           value={formData.title}
         />
+
+        <label>Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+          required={true}
+          value={formData.description}
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Hardware problem">Hardware Problem</option>
+          <option value="Hardware problem">Software Problem</option>
+          <option value="Hardware problem">Network Problem</option>
+          <option value="Hardware problem">Project</option>
+        </select>
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
       </form>
     </div>
   );
